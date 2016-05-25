322a8a5df81907f0b5ed3034f6f3d3f1f3c951d6
Mohammad Iftekhar <meifte12@gmail.com>
2016-05-25 20:43:27 +0530

    changes in parent dao

--- SchoolFly/SchoolFly-App/src/main/java/com/frozan/dao/impl/ParentDaoImpl.java
+++ SchoolFly/SchoolFly-App/src/main/java/com/frozan/dao/impl/ParentDaoImpl.java
@@ -4,10 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.exception.ConstraintViolationException;
+import org.hibernate.exception.JDBCConnectionException;
 import org.springframework.stereotype.Repository;
 
 import com.frozan.dao.interfaces.AbstractDao;
 import com.frozan.dao.interfaces.ParentDao;
+import com.frozan.exception.DaoExceptionMsg;
+import com.frozan.exception.TelentCloudDaoException;
 import com.frozan.hlo.ParentHlo;
 @Repository("parentDao")
 public class ParentDaoImpl extends AbstractDao implements ParentDao {
@@ -18,18 +23,67 @@ public class ParentDaoImpl extends AbstractDao implements ParentDao {
    @Override
 	public boolean save(ParentHlo parentHlo)
 	{
-		persist(parentHlo);
+		try {	
+			logger.debug("ParentHlo is about to be saved : "+parentHlo.getFirstName()+" " );
+			persist(parentHlo);
+			logger.debug("ParentHlo saved succussfully");
+		} catch (JDBCConnectionException e) {
+			logger.error("JDBCConnectionException occured while saving ParentHlo : "+parentHlo.getFirstName()+" " + " : " + e);
+			throw new TelentCloudDaoException(DaoExceptionMsg.CONNECTION_FAILED.getMessage());
+		} catch (ConstraintViolationException e) {
+			logger.error("ConstraintViolationException occured while saving ParentHlo : "+parentHlo.getFirstName()+" "+" : "+ e);
+			throw new TelentCloudDaoException(DaoExceptionMsg.CONSTRAINT_ERROR.getMessage()+" while saving:"+parentHlo.getFirstName()+" "+ e);
+		} catch (HibernateException e) {
+			logger.error("HibernateException occured while saving ParentHlo : "+parentHlo.getFirstName()+" "+" : "+ e);
+			throw new TelentCloudDaoException(DaoExceptionMsg.UPDATE_FAILED.getMessage()+ ": ParentHlo id:"+parentHlo.getFirstName()+" " +e);
+		} catch (Exception e){
+			logger.error("Exception occured while saving ParentHlo : "+parentHlo.getFirstName()+" "+ " : "+ e);
+			throw new TelentCloudDaoException(e.getMessage());
+		}
 		return true;
 	}
    @Override
 	public void modify(ParentHlo parentHlo)
 	{
-		update(parentHlo);
+		try {	
+			logger.debug("ParentHlo is about to be updating : "+parentHlo.getFirstName()+" " );
+			update(parentHlo);
+			logger.debug("ParentHlo updated succussfully");
+		} catch (JDBCConnectionException e) {
+			logger.error("JDBCConnectionException occured while updating ParentHlo : "+parentHlo.getFirstName()+" " + " : " + e);
+			throw new TelentCloudDaoException(DaoExceptionMsg.CONNECTION_FAILED.getMessage());
+		} catch (ConstraintViolationException e) {
+			logger.error("ConstraintViolationException occured while updating ParentHlo : "+parentHlo.getFirstName()+" "+" : "+ e);
+			throw new TelentCloudDaoException(DaoExceptionMsg.CONSTRAINT_ERROR.getMessage()+" while updating:"+parentHlo.getFirstName()+" "+ e);
+		} catch (HibernateException e) {
+			logger.error("HibernateException occured while updating ParentHlo : "+parentHlo.getFirstName()+" "+" : "+ e);
+			throw new TelentCloudDaoException(DaoExceptionMsg.UPDATE_FAILED.getMessage()+ ": ParentHlo id:"+parentHlo.getFirstName()+" " +e);
+		} catch (Exception e){
+			logger.error("Exception occured while updating ParentHlo : "+parentHlo.getFirstName()+" "+ " : "+ e);
+			throw new TelentCloudDaoException(e.getMessage());
+		}
 	}
    @Override
 	public void delete(ParentHlo parentHlo)
 	{
-		delete(parentHlo);
+	   
+		try {	
+			logger.debug("ParentHlo is about to be deleting : "+parentHlo.getFirstName()+" " );
+			delete(parentHlo);
+			logger.debug("ParentHlo deleted succussfully");
+		} catch (JDBCConnectionException e) {
+			logger.error("JDBCConnectionException occured while deleting ParentHlo : "+parentHlo.getFirstName()+" " + " : " + e);
+			throw new TelentCloudDaoException(DaoExceptionMsg.CONNECTION_FAILED.getMessage());
+		} catch (ConstraintViolationException e) {
+			logger.error("ConstraintViolationException occured while deleting ParentHlo : "+parentHlo.getFirstName()+" "+" : "+ e);
+			throw new TelentCloudDaoException(DaoExceptionMsg.CONSTRAINT_ERROR.getMessage()+" while deleting:"+parentHlo.getFirstName()+" "+ e);
+		} catch (HibernateException e) {
+			logger.error("HibernateException occured while deleting ParentHlo : "+parentHlo.getFirstName()+" "+" : "+ e);
+			throw new TelentCloudDaoException(DaoExceptionMsg.UPDATE_FAILED.getMessage()+ ": ParentHlo id:"+parentHlo.getFirstName()+" " +e);
+		} catch (Exception e){
+			logger.error("Exception occured while deleteing ParentHlo : "+parentHlo.getFirstName()+" "+ " : "+ e);
+			throw new TelentCloudDaoException(e.getMessage());
+		}
 	}
    @Override
 	public List<ParentHlo> getParentById(int studentId)
