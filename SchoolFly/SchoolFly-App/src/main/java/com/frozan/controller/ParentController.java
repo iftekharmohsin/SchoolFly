c30d3104c0495ce21e12affdc02f91249eeff12c
Mohammad Iftekhar <meifte12@gmail.com>
2016-06-04 22:01:39 +0530

    some changes

--- SchoolFly/SchoolFly-App/src/main/java/com/frozan/controller/ParentController.java
+++ SchoolFly/SchoolFly-App/src/main/java/com/frozan/controller/ParentController.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-
+                   
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpRequest;
